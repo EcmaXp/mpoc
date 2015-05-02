a7cce5e5d0bf94b8fab3ef4e4256e63bb03e294f
sigsrv <sigsrv@sigsrv.net>
2015-05-02 11:15:10 +0000

    modify bios

--- mpoc-rom/bios.py
+++ mpoc-rom/bios.py
@@ -16,36 +16,42 @@ def dresume(thread, value=None):
       
     print(thread.name, kind, result)
 
-@microthread.auto
-def hello():
+@microthread.auto(3, 2)
+def hello(a, b):
+    print(a, b)
     print(pause(3), pause())
     return 32
 
-#hello()
+print(hello)
 dresume(hello)
 dresume(hello, 32)
 dresume(hello, 31)
 
 import utime
 
+def gen():
+    pass
+
+class sup():
+    def __enter__(self):
+        pause()
+
+    def __exit__(self, a, b, c):
+        pass
+
 for i in range(1, 16 + 1):
-    @microthread.auto
+    @microthread.auto()
     def evil():
         "i am evil!"
-        for k in range(1024 * 1024):
+        for i in range(1024 * 1024 * 1024):
             pass
     
     limit = 1 + i * 1024 * 1024
     evil.cpu_hard_limit = limit
-    #evil.cpu_soft_limit = limit
+    evil.cpu_soft_limit = limit >> 1
     
     a = utime.time()
-    try:
-        print(evil.resume())
-    except:
-        print("hard")
-    else:
-        print("soft")
+    dresume(evil)
     b = utime.time()
     
     print(i, "M =>", round((b - a) * 1000), "ms")
